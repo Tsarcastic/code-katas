3f77e19eb0436e06188939486c8a3e42a9da13c1
Brendan Davis <BrendanMD@gmail.com>
2017-10-21 14:30:41 -0700

    through 05

--- test_cw05_gtmc.py
+++ test_cw05_gtmc.py
@@ -3,48 +3,36 @@ import pytest
 
 def test_01():
     from cw05_gtmc import get_middle
-    assert gimme(get_middle("test") == "es")
+    assert get_middle("test") == "es"
 
 def test_02():
     from cw05_gtmc import get_middle
-    assert gimme(get_middle("test") == "es")
+    assert get_middle("testing") == "t"
 
 def test_03():
     from cw05_gtmc import get_middle
-    assert gimme(get_middle("test") == "es")
+    assert get_middle("middle") == "dd"
 
 def test_04():
     from cw05_gtmc import get_middle
-    assert gimme(get_middle("test") == "es")
+    assert get_middle("A") == "A"
 
 def test_05():
     from cw05_gtmc import get_middle
-    assert gimme(get_middle("test") == "es")
+    assert get_middle("of") == "of"
 
 def test_06():
     from cw05_gtmc import get_middle
-    assert gimme(get_middle("test") == "es")
+    assert get_middle("test") == "es"
 
 def test_07():
     from cw05_gtmc import get_middle
-    assert gimme(get_middle("test") == "es")
+    assert get_middle("test") == "es"
 
 def test_08():
     from cw05_gtmc import get_middle
-    assert gimme(get_middle("test") == "es")
+    assert get_middle("test") == "es"
 
 def test_09():
     from cw05_gtmc import get_middle
-    assert gimme(get_middle("test") == "es")
-
-
-
-
-
-
-
-Test.assert_equals(get_middle("test"),"es")
-Test.assert_equals(get_middle("testing"),"t")
-Test.assert_equals(get_middle("middle"),"dd")
-Test.assert_equals(get_middle("A"),"A")
-Test.assert_equals(get_middle("of"),"of")
+    assert get_middle("test") == "es"
