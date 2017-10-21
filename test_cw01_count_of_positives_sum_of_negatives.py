c239a41b447aec7943dbdcac4a80c23ece9c3198
Brendan Davis <BrendanMD@gmail.com>
2017-10-21 13:21:56 -0700

    Added my own to 01 tests.

--- test_cw01_count_of_positives_sum_of_negatives.py
+++ test_cw01_count_of_positives_sum_of_negatives.py
@@ -4,7 +4,7 @@ import pytest
 
 def test_01():
     from cw01_count_of_positives_sum_of_negatives import count_positives_sum_negatives
-    assert count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]) == [10,-65]
+    assert count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]) == [10, -65]
 
 
 def test_02():
@@ -24,9 +24,25 @@ def test_04():
 
 def test_05():
     from cw01_count_of_positives_sum_of_negatives import count_positives_sum_negatives
-    assert count_positives_sum_negatives([0,0,0,0,0,0,0,0,0]) == [0,0]
+    assert count_positives_sum_negatives([0,0,0,0,0,0,0,0,0]) == [0, 0]
 
 
 def test_06():
     from cw01_count_of_positives_sum_of_negatives import count_positives_sum_negatives
     assert count_positives_sum_negatives([]) == []
+
+def test_07():
+    from cw01_count_of_positives_sum_of_negatives import count_positives_sum_negatives
+    assert count_positives_sum_negatives([1, 1, -2]) == [2, -2]  
+
+def test_08():
+    from cw01_count_of_positives_sum_of_negatives import count_positives_sum_negatives
+    assert count_positives_sum_negatives([5, 7, 8, -5]) == [3, -5]  
+
+def test_09():
+    from cw01_count_of_positives_sum_of_negatives import count_positives_sum_negatives
+    assert count_positives_sum_negatives([100,-5]) == [1, -5]  
+
+def test_10():
+    from cw01_count_of_positives_sum_of_negatives import count_positives_sum_negatives
+    assert count_positives_sum_negatives([-5, -2, -3, 1]) == [1, -10]  
