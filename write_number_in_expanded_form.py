644c9f912559f9fddd47c0659175baffba8762be
Brendan Davis <BrendanMD@gmail.com>
2017-10-21 17:47:46 -0700

    done!!

--- write_number_in_expanded_form.py
+++
@@ -1,9 +0,0 @@
-def expanded_form(num):
-    x = str(num)
-    the_string = ""
-
-    for count, value in enumerate(x):
-        if int(x[count]) > 0:
-            y = x[count] + ('0' * (len(x) - count - 1))
-            the_string += y + " + "
-    return the_string[:-3]
